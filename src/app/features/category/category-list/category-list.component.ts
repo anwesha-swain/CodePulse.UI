656e8296594f70ceaaf0f189ecf942ccc8064f88
anweshaswain-mcd <anwesha.swain@capgemini.com>
2023-11-22 13:03:03 +0530

    Get all categories

--- src/app/features/category/category-list/category-list.component.ts
+++ src/app/features/category/category-list/category-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/categoryModel';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.css'
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit {
 
+  categories$?: Observable<Category[]>;
+
+  constructor(private categoryService: CategoryService) {
+    
+  }
+
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
+  }
+  
 }
--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AddCategoryRequest } from '../models/add-category-request.model';
+import { Category } from '../models/categoryModel';
+import { environment } from '../../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +13,10 @@ export class CategoryService {
   constructor(private http: HttpClient) {}
 
   addCategory(model: AddCategoryRequest): Observable<void> {
-    return this.http.post<void>('https://localhost:7108/api/Categories', model);
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/Categories`, model);
+  }
+
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/Categories`);
   }
 }

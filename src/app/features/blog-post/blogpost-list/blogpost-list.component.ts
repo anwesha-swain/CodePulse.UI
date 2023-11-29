980a362b312a017e23c54e33b2cc2e27613f4666
anweshaswain-mcd <anwesha.swain@capgemini.com>
2023-11-29 11:58:12 +0530

    Get all blog posts.

--- src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
+++ src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { BlogPostService } from '../blog-post.service';
+import { BlogPost } from '../models/blog-post.model';
 
 @Component({
   selector: 'app-blogpost-list',
   templateUrl: './blogpost-list.component.html',
   styleUrl: './blogpost-list.component.css'
 })
-export class BlogpostListComponent {
+export class BlogpostListComponent implements OnInit {
+  blogPosts$?: Observable<BlogPost[]>;
+  constructor(private blogPostService: BlogPostService) {}
+
+  ngOnInit(): void {
+    //get all blog posts from api
+    this.blogPosts$ = this.blogPostService.getAllBlogPosts();
+  }
 
 }
